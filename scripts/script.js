0e3238257bed324cc07a37fff319ccd111285e52
NaashNix <thilakshanaravindu@gmail.com>
2023-09-21 21:44:48 +0530

    Link updated

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@ function searchBtnOnClick(){
    
    $.ajax({
       method : "GET",
-      url : "db/db.txt",
+      url: " http://api.weatherapi.com/v1/current.json?key=a393728118084352b3571035231005&q=Panadura",
       success : (resp) => {
          console.log(resp);
       }
