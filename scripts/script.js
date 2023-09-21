02d10795c539069c902bffc965a09ecd941b08c8
NaashNix <thilakshanaravindu@gmail.com>
2023-09-21 21:50:55 +0530

    Location Text Changing

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,13 @@
+const locationName = $("#locationName");
+
 function searchBtnOnClick(){
    
    $.ajax({
       method : "GET",
-      url: " http://api.weatherapi.com/v1/current.json?key=a393728118084352b3571035231005&q=Panadura",
+      url: " http://api.weatherapi.com/v1/current.json?key=a393728118084352b3571035231005&q=Colombo",
       success : (resp) => {
          console.log(resp);
+         locationName.text(resp.location.name);
       }
    });
    
